eced4e4273b840a4c45e2918454cf7960226f51a
Anirudh Mazumder <AnirudhMazumder@my.unt.edu>
2025-08-08 01:44:05 -0500

    added date handling and a daily sentiment index calculation

--- Daily_Sentiment_Index/Daily_Sentiment_Index_Calculation.py
+++ Daily_Sentiment_Index/Daily_Sentiment_Index_Calculation.py
@@ -1,4 +1,12 @@
 import pandas as pd
+from datetime import datetime, date
+
+def parse_timestamps(time_str):
+    time_obj = pd.to_datetime(time_str, errors='coerce')
+    if pd.notna(time_obj):
+        return time_obj.date()
+    
+    return pd.to_datetime(time_str, errors='coerce')
 
 def loading_financial_data():
     finance_data = pd.read_csv("../Financial_Data_Sentiment_Analysis/Finance_Sentiment_Analysis.csv")
@@ -7,6 +15,7 @@ def loading_financial_data():
         1: 'Neutral',
         2: 'Positive'
     })
+    finance_data['Time'] = finance_data['Time'].apply(parse_timestamps)
     return finance_data
 
 def sentiment_index_calculation(finance_data):
@@ -16,13 +25,13 @@ def sentiment_index_calculation(finance_data):
         'Positive': 1
     }
     finance_data['Sentiment_Score'] = finance_data['Predicted_Class'].map(sentiment_index_values) * finance_data['Confidence']
-    daily_sentiment_index = finance_data.groupby('Time').agg(
-        sentiment_score=('Sentiment_Score', 'mean'),
-        headline_count=('Sentiment_Score', 'count')
-    )
-    return daily_sentiment_index * 100
-
+    
+    daily_sentiment_index = finance_data.groupby('Time')['Sentiment_Score'].mean().mul(100).reset_index()
+    daily_sentiment_index.rename(columns={'Sentiment_Score': 'Daily_Sentiment_Index'}, inplace=True)
+    daily_sentiment_index['Headlines_Processed'] = finance_data.groupby('Time').size().values
+    
+    return daily_sentiment_index
 
 finance_data = loading_financial_data()
 daily_sentiment_index = sentiment_index_calculation(finance_data)
-print(daily_sentiment_index.head())
+print(daily_sentiment_index)
